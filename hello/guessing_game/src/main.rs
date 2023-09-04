71e07c985f5f15890208083fe98595df37f0ef02
wuzhi <1124700977@qq.com>
2023-09-05 07:59:38 +0800

    变量定义

--- hello/guessing_game/src/main.rs
+++ hello/guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数!");
+
+    println!("猜一个数！");
+
+    // let mut foo = 1;
+    // let bar = foo;
+
+    // foo = 2;
+
+    let mut guess=String::new();
+    io::stdin().read_line(&mut guess).expect("无法读取数");
+
+    println!("你猜测的数字为：{}",guess);
 }
